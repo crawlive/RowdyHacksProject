2239d59f43c534ef08c7814fca689e76416ed4a0
kxw830 <ajcool.aj2@gmail.com>
2022-03-26 22:38:59 -0500

    Merge branch 'cait' of https://github.com/crawlive/RowdyHacksProject into aiden
     Conflicts:
    	.idea/misc.xml
    	.idea/vcs.xml
    	app/src/main/java/com/example/rowdyhacks/MainActivity.java

--- app/src/main/java/com/example/rowdyhacks/MainActivity.java
+++ app/src/main/java/com/example/rowdyhacks/MainActivity.java
@@ -1,4 +1,15 @@
 package com.example.rowdyhacks;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
