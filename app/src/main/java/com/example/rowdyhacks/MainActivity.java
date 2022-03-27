4145e13e6c7c66f53e2b36ad20185086bda006a7
kxw830 <ajcool.aj2@gmail.com>
2022-03-26 19:29:12 -0500

    update

--- app/src/main/java/com/example/rowdyhacks/MainActivity.java
+++ app/src/main/java/com/example/rowdyhacks/MainActivity.java
@@ -1,4 +1,20 @@
 package com.example.rowdyhacks;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
 }
---
+++ app/src/main/java/com/example/rowdyhacks/RecyclerViewAdapter.java
@@ -0,0 +1,25 @@
+package com.example.rowdyhacks;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
