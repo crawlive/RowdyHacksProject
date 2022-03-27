b2d44dab655b32e0246b81b7f4dad69c6f3d3a26
kxw830 <ajcool.aj2@gmail.com>
2022-03-26 21:17:27 -0500

    update

--- app/src/main/java/com/example/rowdyhacks/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/rowdyhacks/RecyclerViewAdapter.java
@@ -1,25 +1,65 @@
 package com.example.rowdyhacks;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    final int VIEW_TYPE_LOADING = 0;
+    final int VIEW_TYPE_ITEM = 1;
+    public List<String> mHistory;
+    public RecyclerViewAdapter(List<String> mList){
+        mHistory = mList;
+    }
+
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        if (viewType == VIEW_TYPE_ITEM){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+            return new ItemViewHolder(view);
+        }
+        else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
+            return new LoadingHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+        if(viewHolder instanceof ItemViewHolder)
+            populateItemRows((ItemViewHolder) viewHolder, position);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mHistory == null ? 0: mHistory.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mHistory.get(position) == null ? VIEW_TYPE_LOADING: VIEW_TYPE_ITEM;
+    }
+
+    private class ItemViewHolder extends RecyclerView.ViewHolder {
+        TextView tvItem;
+        public ItemViewHolder(@NonNull View itemView){
+            super(itemView);
+            tvItem = itemView.findViewById(R.id.tvItem);
+        }
+    }
+    private class LoadingHolder extends RecyclerView.ViewHolder {
+        public LoadingHolder (@NonNull View itemView){
+            super(itemView);
+        }
+    }
+    private void populateItemRows (ItemViewHolder viewHolder, int position){
+        String item = mHistory.get(position);
+        viewHolder.tvItem.setText(item);
     }
 }
